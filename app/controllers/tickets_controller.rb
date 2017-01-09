30e21ea1b2983a9a771c73775e76ce69173d2efe
triley22 <triley09@gmail.com>
2017-01-09 11:25:28 -0800

    tickets can now be edited

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,6 +6,16 @@ def new
   @ticket = @project.tickets.build
 end
 
+def update
+  if @ticket.update(ticket_params)
+    flash[:notice] = "Ticket has been updated."
+    redirect_to [@project, @ticket]
+  else
+    flash.now[:alert] = "Ticket has not been updated."
+    render "edit"
+  end
+end
+
 def create
   @ticket = @project.tickets.build(ticket_params)
   
@@ -19,6 +29,9 @@ def create
 end
   
   def show
+  end
+
+  def edit
   end
 
 private
